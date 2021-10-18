fix softmax issue

classifier.py
@@ -56,7 +56,8 @@ class BertSentClassifier(torch.nn.Module):
         classification_layer=torch.nn.Linear(hidden_dimension,self.num_labels)
         raw_label=classification_layer(pooler_output)
         interm_label=self.classification_dropout(raw_label)
-        output= torch.nn.LogSoftmax(interm_label)
+        output_softmax= torch.nn.LogSoftmax()
+        output=output_softmax(interm_label)
 
         return output
         